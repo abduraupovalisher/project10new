2e344221a8d93fb159dd5839c3922d3d2d99a154
abduraupovalisher <102685756+abduraupovalisher@users.noreply.github.com>
2024-11-29 20:59:19 +0600

    Update XMLCompilationEngine.java

--- JackAnalyzer/src/XMLCompilationEngine.java
+++ JackAnalyzer/src/XMLCompilationEngine.java
@@ -46,219 +46,287 @@ public class XMLCompilationEngine implements ICompilationEngine {
     }
 
 
-    @Override
     public void compileClass() {
         write("<class>");
 
         eat("class");
-        // TODO: Implement compileClass method
-        // Hint: The structure of the implementation should be as follows:
-        // 1. Write the opening <class> tag.
-        // 2. Use eat() to consume the 'class' keyword.
-        // 3. Use eat() to consume the class name (identifier).
-        // 4. Use eat() to consume the opening '{' symbol.
-        // 5. Use a while loop to compile class variable declarations (compileClassVarDec()).
-        // 6. Use a while loop to compile subroutine declarations (compileSubroutine()).
-        // 7. Use eat() to consume the closing '}' symbol.
-        // 8. Write the closing </class> tag.
+        eat(TokenType.IDENTIFIER);
+        eat("{");
+        while (jackTokenizer.isClassVarType()) {
+            compileClassVarDec();
+        }
+        while(jackTokenizer.isFunKeyword()){
+            compileSubroutine();
+        }
+        eat("}");
+        write("</class>");
     }
 
-    @Override
     public void compileClassVarDec() {
-        // TODO: Implement compileClassVarDec method
-        // Hint: The structure of the implementation should be as follows:
-        // 1. Write the opening <classVarDec> tag.
-        // 2. Use eat() to consume the keyword (static or field).
-        // 3. Call compileType() to handle the type.
-        // 4. Use eat() to consume the variable name (identifier).
-        // 5. Use a while loop to handle multiple variable names separated by commas.
-        // 6. Use eat() to consume the closing ';' symbol.
-        // 7. Write the closing </classVarDec> tag.
+        write("<classVarDec>");
+// static int a, b, c;
+        eat(TokenType.KEYWORD);
+        compileType();
+        eat(TokenType.IDENTIFIER);
+
+        while(jackTokenizer.tokenType() == TokenType.SYMBOL && jackTokenizer.symbol().equals(",")){
+            eat(",");
+            eat(TokenType.IDENTIFIER);
+        } // field int a, b, c;
+        eat(";"); //int a, b, c;
+        write("</classVarDec>");
     }
 
-    @Override
     public void compileVarDec() {
-        // TODO: Implement compileVarDec method
-        // Hint: The structure of the implementation should be as follows:
-        // 1. Write the opening <varDec> tag.
-        // 2. Use eat() to consume the 'var' keyword.
-        // 3. Call compileType() to handle the type.
-        // 4. Use eat() to consume the variable name (identifier).
-        // 5. Use a while loop to handle multiple variable names separated by commas.
-        // 6. Use eat() to consume the closing ';' symbol.
-        // 7. Write the closing </varDec> tag.
+        write("<varDec>");
+        if (!jackTokenizer.isVarType()) {
+            throw new RuntimeException("expect 'var' to declare variable");
+        }
+        eat(TokenType.KEYWORD);
+        compileType();
+        eat(TokenType.IDENTIFIER);
+
+        while(jackTokenizer.tokenType() == TokenType.SYMBOL && jackTokenizer.symbol().equals(",")){
+            eat(",");
+            eat(TokenType.IDENTIFIER);
+        }
+        eat(";");
+        write("</varDec>");
     }
 
-
-    @Override
     public String compileType() {
-        // TODO: Implement compileType method
-        // Hint: The structure of the implementation should be as follows:
-        // 1. Check if the current token is a primitive type (int, char, boolean) or an identifier (class name).
-        // 2. Use eat() to consume the type token.
-        // 3. Return the type as a string.
-        return null;
+        // int | char | boolean | className
+        if (jackTokenizer.tokenType() == TokenType.IDENTIFIER) {
+            eat(TokenType.IDENTIFIER);
+        } else if (jackTokenizer.isPrimitiveType() ) {
+            eat(TokenType.KEYWORD);
+        } else {
+            throw new RuntimeException("expect 'int' or 'char' or 'boolean' or identifier to declare variable");
+        }
+        return jackTokenizer.getThisToken();
     }
 
-    @Override
     public void compileSubroutine() {
-        // TODO: Implement compileSubroutine method
-        // Hint: The structure of the implementation should be as follows:
-        // 1. Write the opening <subroutineDec> tag.
-        // 2. Use eat() to consume the subroutine keyword (constructor, function, method).
-        // 3. Call compileType() to handle the return type or use eat() to consume 'void'.
-        // 4. Use eat() to consume the subroutine name (identifier).
-        // 5. Use eat() to consume the opening '(' symbol.
-        // 6. Call compileParameterList() to handle the parameter list.
-        // 7. Use eat() to consume the closing ')' symbol.
-        // 8. Call compileSubroutineBody() to handle the subroutine body.
-        // 9. Write the closing </subroutineDec> tag.
+        while (jackTokenizer.isFunKeyword()) {
+            write("<subroutineDec>");
+
+            if (!(jackTokenizer.isFunKeyword())) {
+                throw new RuntimeException("expect 'constructor' or 'function' or 'method' to declare subroutine");
+            }
+            eat(TokenType.KEYWORD);
+            if (jackTokenizer.isPrimitiveType() || jackTokenizer.tokenType() == TokenType.IDENTIFIER) {
+                compileType();
+            } else if (jackTokenizer.getThisToken().equals(KEYWORD_VOID))  {
+                eat(TokenType.KEYWORD);
+            } else {
+                throw new RuntimeException("expect primitive type or empty string to declare subroutine");
+            }
+
+            eat(TokenType.IDENTIFIER);
+            eat("(");
+            // handles empty parameter list and non-empty parameter list
+            if (jackTokenizer.tokenType() == TokenType.SYMBOL && jackTokenizer.symbol().equals(")")) {
+                write("<parameterList>");
+                write("</parameterList>");
+            } else if (jackTokenizer.isPrimitiveType()) {
+                compileParameterList();
+            } else {
+                throw new RuntimeException("expect primitive type or empty string to declare paramList");
+            }
+            eat(")");
+            compileSubroutineBody();
+            write("</subroutineDec>");
+        }
+
     }
 
-    @Override
     public void compileParameterList() {
-        // TODO: Implement compileParameterList method
-        // Hint: The structure of the implementation should be as follows:
-        // 1. Write the opening <parameterList> tag.
-        // 2. Use a while loop to handle multiple parameters separated by commas.
-        // 3. For each parameter, call compileType() and use eat() to consume the parameter name (identifier).
-        // 4. Write the closing </parameterList> tag.
+        write("<parameterList>");
+        //arg type
+        compileType();
+
+        //identifier
+        eat(TokenType.IDENTIFIER);
+
+        while (!(jackTokenizer.tokenType() == TokenType.SYMBOL && jackTokenizer.symbol().equals(")"))) {
+            eat(",");
+            compileType();
+            eat(TokenType.IDENTIFIER);
+        }
+        write("</parameterList>");
     }
 
-
-    @Override
     public void compileSubroutineBody() {
-        // TODO: Implement compileSubroutineBody method
-        // Hint: The structure of the implementation should be as follows:
-        // 1. Write the opening <subroutineBody> tag.
-        // 2. Use eat() to consume the opening '{' symbol.
-        // 3. Use a while loop to handle variable declarations (compileVarDec()).
-        // 4. Call compileStatements() to handle the statements.
-        // 5. Use eat() to consume the closing '}' symbol.
-        // 6. Write the closing </subroutineBody> tag.
+        write("<subroutineBody>");
+
+        eat("{");
+        while (jackTokenizer.tokenType() == TokenType.KEYWORD && jackTokenizer.keyword().equals(KEYWORD_VAR)) {
+            compileVarDec();
+        }
+        compileStatements();
+        eat("}");
+        write("</subroutineBody>");
     }
 
-
-    @Override
     public void compileStatements() {
-        // TODO: Implement compileStatements method
-        // Hint: The structure of the implementation should be as follows:
-        // 1. Write the opening <statements> tag.
-        // 2. Use a while loop to handle different types of statements (let, if, while, do, return).
-        // 3. For each statement type, call the corresponding compile method (compileLet(), compileIf(), compileWhile(), compileDo(), compileReturn()).
-        // 4. Write the closing </statements> tag.
+        write("<statements>");
+        while (jackTokenizer.tokenType() == TokenType.KEYWORD && jackTokenizer.isStatement()) {
+            switch (jackTokenizer.keyword()) {
+                case KEYWORD_LET:
+                    compileLet();
+                    break;
+                case KEYWORD_IF:
+                    compileIf();
+                    break;
+                case KEYWORD_WHILE:
+                    compileWhile();
+                    break;
+                case KEYWORD_DO:
+                    compileDo();
+                    break;
+                case KEYWORD_RETURN:
+                    compileReturn();
+                    break;
+            }
+        }
+        write("</statements>");
     }
 
 
-    @Override
     public void compileLet() {
-        // TODO: Implement compileLet method
-        // Hint: The structure of the implementation should be as follows:
-        // 1. Write the opening <letStatement> tag.
-        // 2. Use eat() to consume the 'let' keyword.
-        // 3. Use eat() to consume the variable name (identifier).
-        // 4. If the next token is '[', handle array indexing by calling compileExpression().
-        // 5. Use eat() to consume the '=' symbol.
-        // 6. Call compileExpression() to handle the expression.
-        // 7. Use eat() to consume the closing ';' symbol.
-        // 8. Write the closing </letStatement> tag.
+        write("<letStatement>");
+        eat("let");
+        eat(TokenType.IDENTIFIER);
+        if(jackTokenizer.tokenType() == TokenType.SYMBOL && jackTokenizer.symbol().equals("[")){
+            eat("[");
+            compileExpression();
+            eat("]");
+        }
+        eat("=");
+        compileExpression();
+        eat(";");
+        write("</letStatement>");
     }
 
-
-    @Override
     public void compileDo() {
-        // TODO: Implement compileDo method
-        // Hint: The structure of the implementation should be as follows:
-        // 1. Write the opening <doStatement> tag.
-        // 2. Use eat() to consume the 'do' keyword.
-        // 3. Use eat() to consume the subroutine call (identifier).
-        // 4. If the next token is '(', handle the subroutine call by calling compileExpressionList().
-        // 5. If the next token is '.', handle the method call by consuming the class name, '.', and subroutine name, then call compileExpressionList().
-        // 6. Use eat() to consume the closing ';' symbol.
-        // 7. Write the closing </doStatement> tag.
+        write("<doStatement>");
+        eat(KEYWORD_DO);
+        eat(TokenType.IDENTIFIER);
+        if(jackTokenizer.tokenType() == TokenType.SYMBOL && jackTokenizer.symbol().equals("(")) {
+            eat("(");
+            compileExpressionList();
+            eat(")");
+        } else if(jackTokenizer.tokenType() == TokenType.SYMBOL && jackTokenizer.symbol().equals(".")){
+            eat(".");
+            eat(TokenType.IDENTIFIER);
+            eat("(");
+            compileExpressionList();
+            eat(")");
+        }
+        eat(";");
+        write("</doStatement>");
     }
 
-    @Override
     public int compileExpressionList() {
-        // TODO: Implement compileExpressionList method
-        // Hint: The structure of the implementation should be as follows:
-        // 1. Write the opening <expressionList> tag.
-        // 2. Use a while loop to handle multiple expressions separated by commas.
-        // 3. For each expression, call compileExpression().
-        // 4. Write the closing </expressionList> tag.
-        // 5. Return the number of expressions in the list.
-        return 0;
+        int count = 0;
+        write("<expressionList>");
+        while (!(jackTokenizer.tokenType() == TokenType.SYMBOL && jackTokenizer.symbol().equals(")"))) {
+            compileExpression();
+            if (jackTokenizer.tokenType() == TokenType.SYMBOL && jackTokenizer.symbol().equals(",")) {
+                eat(",");
+            }
+            count+=1;
+        }
+        write("</expressionList>");
+        return count;
     }
 
-    @Override
     public void compileWhile() {
-        // TODO: Implement compileWhile method
-        // Hint: The structure of the implementation should be as follows:
-        // 1. Write the opening <whileStatement> tag.
-        // 2. Use eat() to consume the 'while' keyword.
-        // 3. Use eat() to consume the opening '(' symbol.
-        // 4. Call compileExpression() to handle the condition expression.
-        // 5. Use eat() to consume the closing ')' symbol.
-        // 6. Use eat() to consume the opening '{' symbol.
-        // 7. Call compileStatements() to handle the statements inside the while loop.
-        // 8. Use eat() to consume the closing '}' symbol.
-        // 9. Write the closing </whileStatement> tag.
+        write("<whileStatement>");
+        eat(KEYWORD_WHILE);
+        eat("(");
+        compileExpression();
+        eat(")");
+        eat("{");
+        compileStatements();
+        eat("}");
+        write("</whileStatement>");
     }
 
-
-    @Override
     public void compileReturn() {
-        // TODO: Implement compileReturn method
-        // Hint: The structure of the implementation should be as follows:
-        // 1. Write the opening <returnStatement> tag.
-        // 2. Use eat() to consume the 'return' keyword.
-        // 3. If the next token is not ';', call compileExpression() to handle the return expression.
-        // 4. Use eat() to consume the closing ';' symbol.
-        // 5. Write the closing </returnStatement> tag.
+        write("<returnStatement>");
+        eat(KEYWORD_RETURN);
+        if(!(jackTokenizer.tokenType() == TokenType.SYMBOL && jackTokenizer.symbol().equals(";"))){
+            compileExpression();
+        }
+        eat(";");
+        write("</returnStatement>");
     }
 
-
-    @Override
     public void compileIf() {
-        // TODO: Implement compileIf method
-        // Hint: The structure of the implementation should be as follows:
-        // 1. Write the opening <ifStatement> tag.
-        // 2. Use eat() to consume the 'if' keyword.
-        // 3. Use eat() to consume the opening '(' symbol.
-        // 4. Call compileExpression() to handle the condition expression.
-        // 5. Use eat() to consume the closing ')' symbol.
-        // 6. Use eat() to consume the opening '{' symbol.
-        // 7. Call compileStatements() to handle the statements inside the if block.
-        // 8. Use eat() to consume the closing '}' symbol.
-        // 9. If the next token is 'else', handle the else block by consuming 'else', '{', calling compileStatements(), and '}'.
-        // 10. Write the closing </ifStatement> tag.
+        write("<ifStatement>");
+        eat(KEYWORD_IF);
+        eat("(");
+        compileExpression();
+        eat(")");
+        eat("{");
+        compileStatements();
+        eat("}");
+        if(jackTokenizer.tokenType() == TokenType.KEYWORD && jackTokenizer.keyword().equals(KEYWORD_ELSE)){
+            eat(KEYWORD_ELSE);
+            eat("{");
+            compileStatements();
+            eat("}");
+        }
+        write("</ifStatement>");
     }
 
-    @Override
     public void compileExpression() {
-        // TODO: Implement compileExpression method
-        // Hint: The structure of the implementation should be as follows:
-        // 1. Write the opening <expression> tag.
-        // 2. Call compileTerm() to handle the first term.
-        // 3. Use a while loop to handle multiple terms separated by operators.
-        // 4. For each operator, use eat() to consume the operator and call compileTerm() to handle the next term.
-        // 5. Write the closing </expression> tag.
+        write("<expression>");
+        compileTerm();
+        while(jackTokenizer.isOp()){
+            eat(TokenType.SYMBOL);
+            compileTerm();
+        }
+        write("</expression>");
     }
 
-
-    @Override
     public void compileTerm() {
-        // TODO: Implement compileTerm method
-        // Hint: The structure of the implementation should be as follows:
-        // 1. Write the opening <term> tag.
-        // 2. Check the type of the current token and handle accordingly:
-        //    - If the token is an integer constant, use eat() to consume it.
-        //    - If the token is a string constant, use eat() to consume it.
-        //    - If the token is a keyword constant, use eat() to consume it.
-        //    - If the token is an identifier, handle variable, array, or subroutine calls.
-        //    - If the token is '(', handle the expression inside parentheses by calling compileExpression().
-        //    - If the token is a unary operator, use eat() to consume it and call compileTerm() to handle the term.
-        // 3. Write the closing </term> tag.
+        write("<term>");
+        if(jackTokenizer.tokenType() == TokenType.INT_CONSTANT){
+            eat(TokenType.INT_CONSTANT);
+        } else if(jackTokenizer.tokenType() == TokenType.STRING_CONSTANT){
+            eat(TokenType.STRING_CONSTANT);
+        } else if(jackTokenizer.tokenType() == TokenType.KEYWORD && jackTokenizer.isKeywordConstant()){
+            eat(TokenType.KEYWORD);
+        } else if(jackTokenizer.tokenType() == TokenType.IDENTIFIER){
+            eat(TokenType.IDENTIFIER);
+            if(jackTokenizer.tokenType() == TokenType.SYMBOL && jackTokenizer.symbol().equals("[")){
+                eat("[");
+                compileExpression();
+                eat("]");
+            } else if(jackTokenizer.tokenType() == TokenType.SYMBOL && jackTokenizer.symbol().equals("(")){
+                eat("(");
+                compileExpressionList();
+                eat(")");
+            } else if(jackTokenizer.tokenType() == TokenType.SYMBOL && jackTokenizer.symbol().equals(".")){
+                eat(".");
+                eat(TokenType.IDENTIFIER);
+                eat("(");
+                compileExpressionList();
+                eat(")");
+
+                // p1.distance(p2)
+            }
+        } else if(jackTokenizer.tokenType() == TokenType.SYMBOL && jackTokenizer.symbol().equals("(")){
+            eat("(");
+            compileExpression();
+            eat(")");
+        } else if(jackTokenizer.isUnaryOp()){
+            eat(TokenType.SYMBOL);
+            compileTerm();
+        }
+        write("</term>");
     }
 
     private void advance() {
